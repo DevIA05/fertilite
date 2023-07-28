fc0fdc6db7de9ebe5bae4d6ba88810881106a841
DevIA05 <dia05@polenumerique.re>
2023-07-28 15:58:14 +0400

    conversion de la valeur en catégorie

--- app/components/API.py
+++ app/components/API.py
@@ -75,13 +75,14 @@ class API_values:
         try:
             response = urllib.request.urlopen(req)
             result = response.read()
-            print(result)
+            result = self.de_transform(result)
         except urllib.error.HTTPError as error:
-            print("The request failed with status code: " + str(error.code))
+            #print("The request failed with status code: " + str(error.code))
             result = str(error.code)
             # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-            print(error.info())
-            print(error.read().decode("utf8", 'ignore'))
+            #print(error.info())
+            #print(error.read().decode("utf8", 'ignore'))
+            
         return result
 
     def transform(self,data):
@@ -113,5 +114,8 @@ class API_values:
         })
         return data
     
-    def de_transform(self,pred):
-        pass
+    def de_transform(self,result):
+        reponse_json = json.loads(result)
+        valeur_obtenue = reponse_json['Results'][0]
+        resultat_transforme = 'Normal' if valeur_obtenue == 0 else 'Altered'
+        return resultat_transforme
